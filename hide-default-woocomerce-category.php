4de307dc4987c23528cd07f448a2d3184c03eb6f
Caleb Burks <19caleb95@gmail.com>
2018-02-11 22:51:52 -0500

    Initial Commit

---
+++ hide-default-woocomerce-category.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Plugin Name: Hide Default WooCommerce Category
+ * Description: Hides the new uncategorized / default category in WooCommerce.
+ * Author: Caleb Burks
+ * Author URI: https://calebburks.com
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * WC tested up to: 3.3
+ * WC requires at least: 3.3
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Hide_Default_WooCommerce_Category' ) ) :
+
+/**
+ * WooCommerce Colors main class.
+ */
+class Hide_Default_WooCommerce_Category {
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @var object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin.
+	 */
+	private function __construct() {
+		if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '3.3', '>=' ) ) {
+			$this->hooks();
+		}
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Includes.
+	 */
+	public function hooks() {
+		// Remove category from main shop page.
+		add_filter( 'woocommerce_product_subcategories_args', array( $this, 'hide_from_main_loop' ) );
+	}
+
+	public function hide_from_main_loop( $args ) {
+		if ( false !== get_option( 'default_product_cat' ) ) {
+			$args['exclude'] = get_option( 'default_product_cat' );
+		}
+
+		return $args;
+	}
+
+}
+
+add_action( 'plugins_loaded', array( 'Hide_Default_WooCommerce_Category', 'get_instance' ) );
+
+endif; // class_exists()
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Hide Default WooCommerce Category
+
+Simple plugin that hides the uncategorized / default category from the main shop loop in WooCommerce 3.3+. Just download, install, and activate this plugin. No settings required.
+
+## TODO
+
+Most (likely all) of these require new hooks to be put into WC core. So will revisit later.
+
+- Hide default category from product meta section.
+- Hide default category from widgets.
+- Hide default category from the [product_categories] shortcode.
+- Hide default category from product permalinks.
